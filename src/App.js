82c4ce630a92de831dfeb38787fc4522a9db9abb
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2022-05-14 14:42:57 +0200

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<h2>Hello</h2>
+			<h3>Hello</h3>
+			<h4>Hello</h4>
+			<h5>Hello</h5>
+			<h6>Hello</h6>
+
+			<p>Hello</p>
+
+			<a href="">Hello</a>
+		</div>
+	);
 }
 
 export default App;
